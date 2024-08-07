52fb98d127e921c8cbe69d57234cb639546e02fd
Thiago Leite <thiagofmleite@gmail.com>
2024-08-07 17:25:08 -0300

    Web Server created

---
+++ cmd/wsrs/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/joho/godotenv"
+	"github.com/thiagoleet/go-ama-api/internal/api"
+	"github.com/thiagoleet/go-ama-api/internal/store/pgstore"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		panic(err)
+	}
+
+	ctx := context.Background()
+
+	pool, err := pgxpool.New(ctx, fmt.Sprintf(
+		"user=%s password=%s host=%s port=%s dbname=%s",
+		os.Getenv("WSRS_DATABASE_USER"),
+		os.Getenv("WSRS_DATABASE_PASSWORD"),
+		os.Getenv("WSRS_DATABASE_HOST"),
+		os.Getenv("WSRS_DATABASE_PORT"),
+		os.Getenv("WSRS_DATABASE_NAME"),
+	))
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer pool.Close()
+	fmt.Println("Server starting...")
+
+	if err := pool.Ping(ctx); err != nil {
+		panic(err)
+	}
+
+	handler := api.NewHandler(pgstore.New(pool))
+
+	go func() {
+		if err := http.ListenAndServe(":8000", handler); err != nil {
+			if !errors.Is(err, http.ErrServerClosed) {
+				panic(err)
+			}
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	fmt.Println("Server stopped...")
+}
---
+++ internal/api/api.go
@@ -0,0 +1,30 @@
+package api
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/thiagoleet/go-ama-api/internal/store/pgstore"
+)
+
+// TODO: change to interface
+type apiHandler struct {
+	q *pgstore.Queries
+	r *chi.Mux
+}
+
+func (h apiHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	h.r.ServeHTTP(w, r)
+}
+
+func NewHandler(q *pgstore.Queries) http.Handler {
+	a := apiHandler{
+		q: q,
+	}
+
+	r := chi.NewRouter()
+
+	a.r = r
+
+	return a
+}
